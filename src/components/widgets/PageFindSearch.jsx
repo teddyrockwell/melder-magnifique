d3414072cde350c7379e8e0a4029098d74a120fc
teddyrockwell <tmelder@xula.edu>
2024-06-08 16:06:37 -0500

    update pagefindsearch.jsx to fix search

--- src/components/widgets/PageFindSearch.jsx
+++ src/components/widgets/PageFindSearch.jsx
@@ -2,30 +2,32 @@ import React, { useEffect } from 'react';
 
 const PageFindSearch = () => {
   useEffect(() => {
-    function initializePagefind() {
-      new PagefindUI({
-        element: '#search',
-        highlightParam: 'highlight',
-        showSubResults: true,
-        pageSize: 5,
-        translations: {
-          placeholder: 'Search',
-          zero_results: 'No results related to [SEARCH_TERM].',
-        },
-        ranking: {
-          pageLength: 0.5,
-          termSimilarity: 0.75,
-          termFrequency: 0.5,
-        },
-      });
+    const initializePagefind = () => {
+      if (!document.querySelector('#search input')) {
+        new PagefindUI({
+          element: '#search',
+          highlightParam: 'highlight',
+          showSubResults: true,
+          pageSize: 5,
+          translations: {
+            placeholder: 'Search',
+            zero_results: 'No results related to [SEARCH_TERM].',
+          },
+          ranking: {
+            pageLength: 0.5,
+            termSimilarity: 0.75,
+            termFrequency: 0.5,
+          },
+        });
 
-      window.addEventListener('keydown', (event) => {
-        if (event.key === '/' || event.key === '.') {
-          event.preventDefault();
-          document.querySelector('div#search input').focus();
-        }
-      });
-    }
+        window.addEventListener('keydown', (event) => {
+          if (event.key === '/' || event.key === '.') {
+            event.preventDefault();
+            document.querySelector('div#search input').focus();
+          }
+        });
+      }
+    };
 
     document.addEventListener('astro:page-load', initializePagefind);
     initializePagefind();
